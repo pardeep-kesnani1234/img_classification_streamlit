356c1f8b3e8bd9f259adf1a39de65e5be25c360d
Pardeep Kumar <pardeep.kesnani@live.com>
2020-05-13 13:12:25 +0500

    updated

--- upload.py
+++ upload.py
@@ -1,9 +1,37 @@
 import streamlit as st 
 from PIL import Image
-from classify import predict
-from classify import init_classify
 import numpy as np
+import cv2
+import PIL
+import numpy
+
 st.title("Let's Classify Image")
+
+def init_classify():
+    global net, classes
+    rows = open('Model/synset_words.txt').read().strip().split("\n")
+    classes = [r[r.find(" ") + 1:].split(",")[0] for r in rows]
+    weight = 'Model/bvlc_googlenet.caffemodel'
+    arch = 'Model/bvlc_googlenet.prototxt'
+    net = cv2.dnn.readNetFromCaffe(arch, weight)
+
+def predict(image1):
+    init_classify()
+    pil_image = PIL.Image.open(image1).convert('RGB')
+    open_cv_image = numpy.array(pil_image) 
+    # Convert RGB to BGR 
+    image = open_cv_image[:, :, ::-1].copy() 	
+    #image = cv2.imread(open_cv_image)
+    # Make prediction
+    blob = cv2.dnn.blobFromImage(cv2.resize(image, (224, 224),
+                                                (104, 117, 123)))
+    net.setInput(blob)
+    prediction = net.forward()
+    idxs = np.argsort(prediction[0])[::-1][0]
+    label = classes[idxs]
+    confidence = round(prediction[0][idxs] * 100, 2)
+    #comb = label + "," + str(confidence)
+    return label,confidence
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
